777f95a545fbdbeba4e8caaad7597adff22eea1d
Denzel Nnodimele <denzelrbx@gmail.com>
2020-12-13 20:29:07 +0100

    test

---
+++ loader.lua
@@ -0,0 +1,7 @@
+local place_ids = {
+    ["Anime Battle Arena"] = 1458767429;
+}
+
+if game.PlaceId == place_ids["Anime Battle Arena"] then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/musgraviite/GraviteHub/main/scripts/animebattlearena.lua", true))()
+end
---
+++ scripts/animebattlearena.lua
@@ -0,0 +1,4 @@
+while true do
+    wait(2)
+    print("aighthththththhthththt")
+end
